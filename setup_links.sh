5f6ce04e065bd0039ca3dc68ace650b97b2d1ca7
Martin Zielinski <darthcoder1@gmail.com>
2019-03-23 00:08:27 +0100

    - Added setup script  - Added git-prompt for bash

---
+++ setup_links.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+SCRIPT_PATH=$(realpath $0)
+BASE_PATH=$(dirname $SCRIPT_PATH)
+
+setup_symlink() {
+    ln -sv $BASE_PATH/$1 ~/$1
+}
+
+
+# Initialize the symlinks for the dotfiles
+setup_symlink .bash/.bash_profile
+setup_symlink .bash/.git-prompt.sh
