7c3a9b03d53a1d051d62559ca5b5f9a9c5e74bf7
Martin Zielinski <darthcoder1@gmail.com>
2019-03-23 14:52:48 +0100

    Install vscode extesions script

--- vscode_install_extensions.sh
+++ vscode_install_extensions.sh
@@ -2,5 +2,8 @@
 
 BASE_PATH=$(pwd)
 
-EXT_FILE=$BASE_PATH.vscode/extensions.txt
-echo $EXT_FILE
+EXT_FILE=$BASE_PATH/.vscode/extensions.txt
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    code --install-extension $line
+done < "$EXT_FILE"
