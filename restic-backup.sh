704f93d4eea9e22660125e8131ec50a42be1a663
Martin Zielinski <martin@yaak.ai>
2021-12-25 18:38:16 +0100

    fixup user and use absolyte path for restic, as /usr/local/bin is not in PATH on macos

--- restic-backup.sh
+++ restic-backup.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 # invoke this script from crontab daily at 5am
 
-RESTIC_REPOSITORY=sftp:dc1-desktop@dc1-home:/dc1-desktop
+RESTIC_REPOSITORY=sftp:dc1-mbp@dc1-home:/data/backups/dc1-mbp
 RESTIC_PASSWORD=babyhearing
 
 echo $RESTIC_PASSWORD >/tmp/password
 
 # perform the back up
-restic -r $RESTIC_REPOSITORY --password-file /tmp/password backup \
-    ~/Documents \
-    ~/Pictures/ \
-    ~/Videos/ \
-    ~/Music \
-    ~/Desktop
+/usr/local/bin/restic -r $RESTIC_REPOSITORY --password-file /tmp/password backup \
+    /Users/$USER/Documents \
+    /Users/$USER/Movies \
+    /Users/$USER/Music
 
 # clean up the snapshots and keep daily for a week, weekly
 # for a month, monthly for a year and yearly for 75 years
-restic -r $RESTIC_REPOSITORY --password-file /tmp/password forget \
+/usr/local/bin/restic -r $RESTIC_REPOSITORY --password-file /tmp/password forget \
     --keep-daily 7 \
     --keep-weekly 5 \
     --keep-monthly 12 \
